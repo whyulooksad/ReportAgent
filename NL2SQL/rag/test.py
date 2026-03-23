69c13e108426fa307e158a995b2c99b5b9ea4f1a
Shaotw1230 <3262818908@qq.com>
2026-03-23 14:28:07 +0800

    2026.2.23

--- NL2SQL/rag/test.py
+++
@@ -1,9 +0,0 @@
-from NL2SQL.rag.loader import parse_schema_md_by_table
-
-with open("D:/Work/sqldata/docs/schema_doc.md", encoding="utf-8") as f:
-    md_text = f.read()
-
-entries = parse_schema_md_by_table(md_text)
-print(" 表总数：", len(entries))
-for entry in entries:
-    print(" 表名:", entry["metadata"]["table"])
